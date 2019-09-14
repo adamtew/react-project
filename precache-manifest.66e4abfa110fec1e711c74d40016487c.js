5a30b99c6bc91a6b7139aa3b9fc4be8ad0e77c0a
Adam <adamjtew@gmail.com>
2019-09-13 20:15:47 -0600

    Updates

---
+++ precache-manifest.66e4abfa110fec1e711c74d40016487c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c09e3e0464178fd19c07368270c9be",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "cbe2c50ec21217c8d206",
+    "url": "/react-project/static/css/main.65e4c07c.chunk.css"
+  },
+  {
+    "revision": "be6726109f43718b8802",
+    "url": "/react-project/static/js/2.8fa8d9e3.chunk.js"
+  },
+  {
+    "revision": "cbe2c50ec21217c8d206",
+    "url": "/react-project/static/js/main.8faa2082.chunk.js"
+  },
+  {
+    "revision": "02cbc43e3abfda1b84f3",
+    "url": "/react-project/static/js/runtime~main.4e4cbac7.js"
+  },
+  {
+    "revision": "d841ee775fb76d2d42afa4bc5c90e493",
+    "url": "/react-project/static/media/header.d841ee77.png"
+  }
+]);
